6c91f2e66b1a42dfb59eb4eec0e6b40a225d9e62
Lucas Antoniazzi <35701477+lcantoniazzi@users.noreply.github.com>
2025-11-30 14:55:05 -0500

    Update Microbot.java

--- src/test/java/net/runelite/client/Microbot.java
+++ src/test/java/net/runelite/client/Microbot.java
@@ -4,17 +4,29 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import net.runelite.client.plugins.fishing.FishingPlugin;
-import net.runelite.client.plugins.microbot.aiofighter.AIOFighterPlugin;
-import net.runelite.client.plugins.microbot.astralrc.AstralRunesPlugin;
-import net.runelite.client.plugins.microbot.autofishing.AutoFishingPlugin;
-import net.runelite.client.plugins.microbot.example.ExamplePlugin;
+import net.runelite.client.plugins.microbot.agility.MicroAgilityPlugin;
+import net.runelite.client.plugins.devtools.DevToolsPlugin;
+import net.runelite.client.plugins.loginscreen.LoginScreenPlugin;
+import net.runelite.client.plugins.microbot.accountselector.AutoLoginPlugin;
+import net.runelite.client.plugins.microbot.barrowsGandolf.BarrowsPlugin;
+import net.runelite.client.plugins.microbot.birdhouseruns.FornBirdhouseRunsPlugin;
+import net.runelite.client.plugins.microbot.moonsofperil.MoonsOfPerilPlugin;
+import net.runelite.client.plugins.microbot.qualityoflife.QoLPlugin;
+import net.runelite.client.plugins.microbot.thieving.ThievingPlugin;
 
 public class Microbot
 {
 
 	private static final Class<?>[] debugPlugins = {
-		AIOFighterPlugin.class
+		ThievingPlugin.class,
+            FornBirdhouseRunsPlugin.class,
+            LoginScreenPlugin.class,
+            AutoLoginPlugin.class,
+            QoLPlugin.class,
+            DevToolsPlugin.class,
+            MoonsOfPerilPlugin.class,
+            MicroAgilityPlugin.class,
+            BarrowsPlugin.class,
 	};
 
     public static void main(String[] args) throws Exception
